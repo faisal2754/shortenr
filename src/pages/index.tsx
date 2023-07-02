93699673eef7396d1a04b7bf9641c3e926c6258a
Faisal Saleem <saleemfs27@gmail.com>
2023-07-02 15:04:35 +0200

    Basic layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
+  const [isShortened, setIsShortened] = useState(false);
+
   return (
     <>
       <Head>
@@ -8,8 +11,24 @@ export default function Home() {
         <meta name="description" content="Shorten any web link for free" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div>Hi</div>
+      <main className="relative flex min-h-screen flex-col items-center justify-center bg-[url('/bg.webp')] font-nunito">
+        {/* Header */}
+        <div className="absolute top-10 text-5xl font-bold">Shortenr</div>
+        {/* Content */}
+        <div>
+          <form className="flex gap-1">
+            <input
+              type="text"
+              className="w-80 rounded-md bg-transparent px-2 py-1 shadow-2xl outline outline-1 backdrop-blur-2xl hover:shadow-black focus:shadow-black"
+            />
+            <button
+              className="w-32 rounded-md border-none bg-black px-3 py-2 text-white shadow-lg"
+              type="submit"
+            >
+              Shorten
+            </button>
+          </form>
+        </div>
       </main>
     </>
   );
